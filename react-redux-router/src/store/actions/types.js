f093505294a1f2e300d4ab6cf15deccb3e1196c4
justin lee <yuminjustin@163.com>
2017-10-19 15:23:34 +0800

    actionType

---
+++ react-redux-router/src/store/actions/types.js
@@ -0,0 +1,6 @@
+/* common */
+export const NUM_HREF = 'num_href';
+export const SIMULATION_LOGIN = 'simulation_login'
+/* agent  下面两个 是示例 实际没有使用 */
+export const AGENT_CHANGE_TYPE = 'agent_change_type';
+export const AGENT_CHANGE_DATE = 'agent_change_date';
