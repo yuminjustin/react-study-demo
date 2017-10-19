20be71931359800bd4ac7cb26bac1aa3e36a4870
justin lee <yuminjustin@163.com>
2017-10-19 15:17:16 +0800

    菜单数据
    此处为静态，实际项目中可以是接口获取

---
+++ react-redux-router/src/components/bar/menu.js
@@ -0,0 +1,22 @@
+export default [{
+        name: 'Agent管理',
+        icon: 'team',
+        key: 1,
+        children: [{
+                key: '1-1',
+                name: 'Agent列表',
+                url: '/agentList'
+            },
+            {
+                key: '1-2',
+                name: '测试菜单',
+                url: '/test2'
+            },
+            {
+                key: '1-3',
+                name: '404测试',
+                url: '/test3'
+            }
+        ]
+    }
+]
